f6785fc28dfd31c6ddc3cec17fe1ee37dbfc2c00
MisterPrestoGamer <prestogamer12345@gmail.com>
2025-09-26 21:19:27 +0100

    update footer with github link and email with copy on click func

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,32 @@
 import styles from "./footer.module.css";
+import { Copy } from "lucide-react";
 
 export default function Footer({ darkmode }) {
+  async function setClipboard(event) {
+    const type = "text/plain";
+    const clipboardItemData = {
+      [type]: event.target.textContent,
+    };
+    const clipboardItem = new ClipboardItem(clipboardItemData);
+    await navigator.clipboard.write([clipboardItem]);
+  }
+
   return (
     <div className={darkmode ? `${styles.footer} altDarkmode` : styles.footer}>
-      <div className={styles.contactInfo}>contact info</div>
-      <div className={styles.portrait}>portrait</div>
+      <div className={styles.contactInfo}>
+        <button onClick={setClipboard} className={styles.email}>
+          milesnelwork1@gmail.com
+          <Copy size={"1.5vw"} />
+        </button>
+
+        <a
+          className={styles.github}
+          href="https://github.com/MisterPrestoGamer"
+          target="_blank"
+        >
+          github
+        </a>
+      </div>
     </div>
   );
 }
