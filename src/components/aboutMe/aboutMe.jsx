2601ad376c343ef1d7b17d1dceba0c42b13ade50
kon65138 <156806318+kon65138@users.noreply.github.com>
2025-11-24 23:33:11 +0000

    add a tiny amout of text to about me page lmao

--- src/components/aboutMe/aboutMe.jsx
+++ src/components/aboutMe/aboutMe.jsx
@@ -7,10 +7,7 @@ export default function AboutMe() {
     <div className={darkmode ? `${styles.aboutMe} darkmode` : styles.aboutMe}>
       <h2 className={styles.title}>About Me</h2>
       <div className={styles.description}>
-        {" "}
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit facere
-        odit veritatis molestiae a itaque nihil amet, maxime sed aliquam aliquid
-        voluptatum similique commodi vel tempora quo, ea deleniti repudiandae?
+        Hi my name is Miles, I'm a 20yr old self-taught full stack webdev based out of Berkshire, England.
       </div>
     </div>
   );
